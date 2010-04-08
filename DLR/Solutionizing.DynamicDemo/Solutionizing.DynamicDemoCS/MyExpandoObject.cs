42d480441325a7f8afa53a9ba31b864913f12f58
dahlbyk <dahlbyk@gmail.com>
2010-04-07 21:03:23 -0500

    Dynamic .NET Demystified - Iowa Code Camp 2009.11

---
+++ DLR/Solutionizing.DynamicDemo/Solutionizing.DynamicDemoCS/MyExpandoObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace Solutionizing.DynamicDemoCS
+{
+    class MyExpandoObject : DynamicObject
+    {
+        private Dictionary<string, object> _data = new Dictionary<string, object>();
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return _data.TryGetValue(binder.Name, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _data[binder.Name] = value;
+            return true;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            dynamic target = _data[binder.Name];
+            if (target == null)
+                throw new InvalidOperationException("Could not invoke member.");
+            switch (args.Length)
+            {
+                case 0: result = target();
+                    return true;
+                case 1: result = target(args[0]);
+                    return true;
+                default: throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ DLR/Solutionizing.DynamicDemo/Solutionizing.DynamicDemoCS/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Solutionizing.DynamicDemoCS
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic ex = new MyExpandoObject();
+
+            ex.Name = "Keith";
+            ex.BirthDate = new DateTime(2008, 1, 1);
+
+            ex.ToString2 = new Func<string>(() => ex.Name + " " + ex.BirthDate);
+
+
+            Console.WriteLine(ex.ToString2());
+
+            Console.ReadKey();
+        }
+
+        public static dynamic GetRandom()
+        {
+            if (DateTime.Now.Second % 2 == 0)
+                return "hello";
+            else
+                return new[] { "hello", "goodbye" };
+        }
+        
+
+
+        #region RandomTypeDemo
+
+        static void RandomTypeDemo()
+        {
+            foreach (var i in System.Linq.Enumerable.Range(0, 16))
+            {
+                dynamic x = GetDynamicX(i);
+                dynamic y = GetDynamicY(i);
+
+                dynamic z = null;
+                try
+                {
+                    z = x + y + 5;
+                }
+                catch (Exception ex)
+                {
+                    z = ex.Message;
+                }
+
+                Console.WriteLine("{0}\n    {1} + {2} = {3}", z.GetType(), x, y, z);
+                if ('q' == Console.ReadKey().KeyChar)
+                    break;
+            }
+        }
+
+        static dynamic GetDynamicX(int i)
+        {
+            switch (i % 4)
+            {
+                case 0: return 1;
+                case 2: return "XX";
+                case 1: return 1.5;
+                default: return DateTime.Now;
+            }
+        }
+
+        static dynamic GetDynamicY(int i)
+        {
+            switch (i / 4)
+            {
+                case 0: return 2;
+                case 1: return 3.14m;
+                case 2: return new TimeSpan(12, 0, 0);
+                default: return "YY";
+            }
+        }
+
+        #endregion
+
+        #region ExpandoDemo
+
+        public static void ExpandoDemo()
+        {
+            var expandos = new dynamic[] {
+                new ExpandoObject(),
+                new MyExpandoObject()
+            };
+
+            foreach (var ex in expandos)
+            {
+                ex.Name = "Keith";
+
+                Console.WriteLine(ex.Name);
+
+                ex.Increment = new Func<int, int>(i => i + 1);
+                Console.WriteLine("1 + 1 = {0}", ex.Increment(1));
+            }
+        }
+
+        #endregion
+    }
+}
