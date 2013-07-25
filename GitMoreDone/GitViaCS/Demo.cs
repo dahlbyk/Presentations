575f63f113d416cb8d47bbf7b50fda0ad08c2303
Keith Dahlby <dahlbyk@gmail.com>
2013-07-25 15:04:42 -0500

    Set up sandbox

---
+++ GitMoreDone/GitViaCS/Demo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LibGit2Sharp;
+
+namespace GitViaCS
+{
+    public static class Demo
+    {
+        static void New()
+        {
+            var newRepoPath = Repository.Init(@"C:\Temp\GitViaCS\{0:hhmmss}".FormatWith(DateTime.Now));
+
+            using (var repo = new Repository(newRepoPath))
+            {
+                repo.Info.IsHeadOrphaned.Dump();
+            }
+        }
+
+        static void Glimpse()
+        {
+            using (var repo = new Repository(@"C:\Dev\GitHub\Glimpse"))
+            {
+                Console.WriteLine();
+                Console.WriteLine("=======================");
+                Console.WriteLine("Refs");
+                Console.WriteLine("=======================");
+                repo.Refs.Dump();
+            }
+        }
+    }
+}
---
+++ GitMoreDone/GitViaCS/Xtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace GitViaCS
+{
+    static class Xtensions
+    {
+        public static void Dump<T>(this T @this)
+        {
+            if (@this is IEnumerable && !(@this is string))
+            {
+                foreach (var x in (IEnumerable)@this)
+                    x.Dump();
+            }
+            else
+            {
+                Console.WriteLine(ReferenceEquals(@this, null) ? "(null)" : @this.ToString());
+            }
+        }
+
+        public static void Dump<T>(this T @this,
+                                   string format,
+                                   IFormatProvider formatProvider = null)
+            where T : IFormattable
+        {
+            Console.WriteLine(ReferenceEquals(@this, null) ? "(null)"
+                            : @this.ToString(format, formatProvider));
+        }
+
+        public static string FormatWith(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+    }
+}
